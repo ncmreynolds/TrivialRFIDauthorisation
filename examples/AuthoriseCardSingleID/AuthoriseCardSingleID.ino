a266842f94742c08972eb098bcc257d629943f38
Nick Reynolds <ncmreynolds@googlemail.com>
2023-08-14 11:28:46 +0100

    Add example for authorising a single ID and wipe all others

---
+++ examples/AuthoriseCardSingleID/AuthoriseCardSingleID.ino
@@ -0,0 +1,59 @@
+/*
+ * Example of the Trivial RFID authorisation library
+ * 
+ * https://github.com/ncmreynolds/TrivialRFIDauthorisation
+ * 
+ * This example will write authorisation for a single ID to the card when presented to the reader
+ * 
+ * 
+ */
+#include <TrivialRFIDauthorisation.h>
+
+#if defined(ARDUINO_AVR_NANO)
+TrivialRFIDauthorisation rfid(10);  //RFID reader CS is connected to pin 10 of an Arduino Nano
+#elif defined(ARDUINO_ESP8266_WEMOS_D1MINI) || defined(ARDUINO_ESP8266_WEMOS_D1MINIPRO)
+TrivialRFIDauthorisation rfid(D8);  //RFID reader CS is connected to pin D8 of a WeMos D1 mini
+#elif defined(ARDUINO_ESP32S2_DEV)
+TrivialRFIDauthorisation rfid(38);  //RFID reader CS is connected to pin 38 of a ESP32S2-Saola-1
+#endif
+
+uint8_t idToAuthorise = 28;
+
+void setup() {
+  Serial.begin(115200);
+  //rfid.debug(Serial); //Enables debugging. Doing this before 'begin' means you can see the reader initialisation information!
+  if(rfid.begin()) { //Start the RFID reader
+    Serial.println(F("*******************************"));
+    Serial.print(F("Starting authorisation of cards for ID:"));
+    Serial.print(idToAuthorise);
+    Serial.println(F(" only\r\n*******************************"));
+  }
+  else
+  {
+    Serial.println(F("*******************************"));
+    Serial.println(F("RFID reader self test failed"));
+    Serial.println(F("*******************************"));
+    delay(0);
+  }
+}
+
+void loop() {
+  rfid.pollForCard(); //Must run regularly to read and process the card
+  if(rfid.cardPresent() == true && rfid.cardChanged() == true)
+  {
+    bool result = rfid.authoriseCard(idToAuthorise, false);  //Authorises the card for a single ID, wiping any other authorisations
+    Serial.println(F("*******************************"));
+    Serial.print(F("Authorising card for ID:"));
+    Serial.print(idToAuthorise);
+    Serial.print(F(" only - "));
+    if(result == true)
+    {
+      Serial.println(F("success"));
+    }
+    else
+    {
+      Serial.println(F("failure"));
+    }
+    Serial.println(F("*******************************"));
+  }
+}
